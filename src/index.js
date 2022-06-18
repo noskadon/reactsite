5d539b65e0fbe086f04c244b7861127fe6cab2a8
Don Noska <noska.don@gmail.com>
2022-06-18 10:55:54 -0500

    Router setup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { BrowserRouter } from "react-router-dom";
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
@@ -16,7 +17,9 @@ const root = createRoot(container);
 root.render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<App />
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
 		</Provider>
 	</React.StrictMode>
 );
