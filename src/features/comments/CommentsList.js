b3711f75629fbfbac7e2c843ccefbe08b8d93704
Don Noska <noska.don@gmail.com>
2022-06-25 22:37:44 -0500

    a few adjustments

--- src/features/comments/CommentsList.js
+++ src/features/comments/CommentsList.js
@@ -1,6 +1,8 @@
 import { Col } from "reactstrap";
 import Comment from "./Comment";
 import { selectCommentsByCampsiteId } from "./commentsSlice";
+import CommentForm from "./CommentForm";
+
 const CommentsList = ({ campsiteId }) => {
 	const comments = selectCommentsByCampsiteId(campsiteId);
 
@@ -11,6 +13,7 @@ const CommentsList = ({ campsiteId }) => {
 				{comments.map((comment) => {
 					return <Comment key={comment.id} comment={comment} />;
 				})}
+				<CommentForm campsiteId={campsiteId} />
 			</Col>
 		);
 	}
