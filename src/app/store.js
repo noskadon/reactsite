9c8f9285582d64eaab733812626f382fbf644570
Don Noska <noska.don@gmail.com>
2022-09-10 10:53:59 -0500

    Redux for All

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { campsitesReducer } from "../features/campsites/campsitesSlice";
+import { commentsReducer } from "../features/comments/commentsSlice";
+import { partnersReducer } from "../features/partners/partnersSlice";
+import { promotionsReducer } from "../features/promotions/promotionsSlice";
+import logger from "redux-logger";
+
 
 export const store = configureStore({
-	reducer: {
-		counter: campsitesReducer,
-	},
+  reducer: {
+    campsites: campsitesReducer, 
+    comments: commentsReducer,
+    partners: partnersReducer,
+    promotions: promotionsReducer
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([logger]),
 });
--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -4,13 +4,14 @@ import { selectFeaturedCampsite } from "../campsites/campsitesSlice";
 import { selectFeaturedPromotion } from "../promotions/promotionsSlice";
 import { selectFeaturedPartner } from "../partners/partnersSlice";
 import AnimatedDisplayCard from "./AnimatedDisplayCard";
+import { useSelector } from "react-redux";
 
 const DisplayList = () => {
-	const items = [
-		selectFeaturedCampsite(),
-		selectFeaturedPromotion(),
-		selectFeaturedPartner(),
-	];
+	const items = useSelector((state) => [
+		selectFeaturedCampsite(state),
+		selectFeaturedPromotion(state),
+		selectFeaturedPartner(state),
+	]);
 
 	return (
 		<Row>
