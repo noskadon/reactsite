32d78abb471c67c4a450420221cac0e2e471332f
Don Noska <noska.don@gmail.com>
2022-09-17 10:44:50 -0500

    Async Campsites

--- src/App.js
+++ src/App.js
@@ -7,8 +7,16 @@ import Footer from "./components/Footer";
 import CampsiteDetailPage from "./pages/CampsiteDetailPage";
 import AboutPage from "./pages/AboutPage";
 import "./App.css";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchCampsites } from "./features/campsites/campsitesSlice";
 
 function App() {
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		dispatch(fetchCampsites());
+	}, [dispatch]);
 	return (
 		<div className="App">
 			<Header />
